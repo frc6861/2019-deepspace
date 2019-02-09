e6b2dd9c996a802f2959fd645b45abcfb94d7bcd
JackWagnerTyro <jwagner@livoniawarriors.org>
2019-02-08 19:28:32 -0500

    solenoids for wedge pickup at bottom is working now

--- src/main/java/frc/tyros/subsystems/Ingestor.java
+++ src/main/java/frc/tyros/subsystems/Ingestor.java
@@ -3,87 +3,51 @@ package frc.tyros.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.XboxController;
 
 import frc.tyros.OI;
-
 
 /**
  * An example subsystem. You can replace me with your own Subsystem.
  */
 public class Ingestor extends Subsystem {
-  private WPI_TalonSRX IngestorLeft;
-  private WPI_TalonSRX IngestorRight;
-  private Compressor compressor;
-  private Solenoid solenoidW1;
-  private Solenoid solenoidW2;
+    private WPI_TalonSRX leftMotor;
+    private WPI_TalonSRX rightMotor;
+    private Compressor compressor;
+    private DoubleSolenoid solenoidW;
+    private DoubleSolenoid solenoidL;
 
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
 
+    public Ingestor() {
+        compressor = new Compressor(0);
+        leftMotor = new WPI_TalonSRX(13);
+        rightMotor = new WPI_TalonSRX(2);
+        solenoidW = new DoubleSolenoid(1, 2);
+        solenoidL = new DoubleSolenoid(3, 4); // TODO get channel number, rename
+        compressor.start();
+    }
 
-  public static void Ingestor() {
-    compressor = new Compressor(0);
-    leftMotor = new WPI_TalonSRX(13);
-    rightMotor = new WPI_TalonSRX(2);
     
+    public void extend() {
+        solenoidW.set(DoubleSolenoid.Value.kForward);
 
+    }
 
-    boolean doubleK;
-    boolean enabled = compressor.enabled();
-    boolean pressureSwitch = compressor.getPressureSwitchValue();
-    double current = compressor.getCompressorCurrent();
-    DoubleSolenoid solenoidW = new DoubleSolenoid(1,2);
-    boolean left = new getLeftSolenoid();
-    boolean right = new getRightSolenoid();
+    public void retract() {
+        solenoidW.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+
+    }
+
     
-    SolenoidW(port, port); {
-      boolean doubleK = true;
-      left = new Solenoid(slot, port1);
-      right = new Solenoid(slot, port2);
-      System.out.println("Double MySolenoid enabled on ports " + port1 + " and " + port2 + "");
-    }
-
-    extend(); {
-      left.set(true);
-      right.set(true);
-
-      if(doubleK) {
-        left.set(false);
-        right.set(false);
-      }
-    }
-
-    retract(); {
-      left.set(false);
-      right.set(false);
-
-      if(doubleK) {
-        left.set(true);
-        right.set(true);
-      }
-    }
-  
-  
-    
-    public boolean getLeftSolenoid() {
-      return left.get();
-    }
-
-    public boolean getRightSolenoid() {
-      boolean otherLeft = right.get();
-      return right.get();
-    }
-
-  }
-   
-
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-
-  }
 }
